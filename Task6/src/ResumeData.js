a21e47de403895e9f104b2cf80ce5a5c208a819d
Shraddha Pandey <85031120+shraddhapandey100@users.noreply.github.com>
2023-11-11 19:56:04 +0530

    Add files via upload

---
+++ Task6/src/ResumeData.js
@@ -0,0 +1,99 @@
+let resumeData = {
+   
+   "name":"Shraddha",
+   "role":"Student of Cyber Security and Software Engineering",
+   "roleDescription":"I like to learn and create Software Application While maintaining Security Postures. In my free time I do Web Penetration Testing and Learning New Skills.",
+   "socialLinks":[
+    {
+   "name":"linkedin",
+   "url":"https://www.linkedin.com/in/pshraddha30/",
+   "className":"fa fa-linkedin"
+    },
+    {
+   "name":"github",
+   "url":"https://github.com/shraddhapandey100/",
+   "className":"fa fa-github"
+    }
+    ],
+   "aboutme":"I am a pre-final year student and currently pursuing M.Tech(Integrated) Computer Science & Engineering Specialization in Cyber Security from Vellore Institute of Technology-Bhopal. ",
+   "address":"India",
+   "website":"https://github.com/shraddhapandey100/",
+   "education":[
+    {
+   "UniversityName":"Vellore Institute of Technology - Bhopal",
+   "specialization":"Cyber Securiy",
+   "MonthOfPassing":"July",
+   "YearOfPassing":"2020-Present",
+   "Achievements":"100% Scholarship STARS Student || 8.75 CGPA"
+    },
+    {
+   "UniversityName":"Girls H.S School Devendranagar Panna (M.P.)",
+   "specialization":"Intermediate - PCM",
+   "MonthOfPassing":"July",
+   "YearOfPassing":"2020",
+   "Achievements":"95.00 % in 12th Class - Distt. Topper"
+    }
+    ],
+   "work":[
+    {
+   "CompanyName":"GeeksforGeeks",
+   "specialization":"Internship - Technical Content Writer",
+   "MonthOfLeaving":"Jan-April",
+   "YearOfLeaving":"2023",
+   "Achievements":"Articles Publications Link- https://www.geeksforgeeks.org/what-are-some-important-practices-to-secure-an-angular-application/ "
+    },
+    {
+   "CompanyName":"Cyber Security global virtual Internship",
+   "specialization":"Internship - Information Security Administrator",
+   "MonthOfLeaving":"may-june",
+   "YearOfLeaving":"2022",
+   "Achievements":"Digital Forensics | Practical Ethical Hacking | Information Security"
+    }
+    ],
+   "skillsDescription":"Skills",
+   "skills":[
+    {
+   "skillname":"Java"
+    },
+    {
+   "skillname":"Python"
+    }
+    ,
+    {
+   "skillname":"Machine_Learning"
+    },
+    {
+   "skillname":"MERN_Full_Stack"
+    }
+    ,
+    {
+   "skillname":"Cloud_Computing_AWS_Cloud"
+    },
+    {
+   "skillname":"Digital Forensics"
+    }
+    ],
+   "portfolio":[
+    {
+   "name":"Project1",
+   "description":"Dengurd Antivirus: for finding the malicious Program",
+   "imgurl":"images/portfolio/ProjectOne.jpg"
+    },
+    {
+   "name":"Project2",
+   "description":"The red Hat Web Application Scanner",
+   "imgurl":"images/portfolio/ProjectTwo.jpg"
+    }
+    ],
+   "testimonials":[
+    {
+   "description":"This is Portfolio of Shraddha Pandey",
+   "name":"Software Engineering || Cyber Security || Machine Learning"
+    },
+    {
+   "description":"Drawing, Playing BatMinton, Research",
+   "name":"Innovation || Work Hard"
+    }
+    ]
+    }
+    export default resumeData
